bebf7c1160357f7a2f7b9a375c2ea9e9a92b5712
Hector Franco Torres <tor6.franco6@hotmail.com>
2019-12-22 18:14:42 -0600

    Operadores de asignacion

---
+++ OperadoresDeAsignacion/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Declaramos variables
+    int x = 10;
+
+    // x = x + 2x
+    x += 2 * x;
+
+    // Mostramos el resultado
+    printf("X vale: %i", x);
+
+    return 0;
+}
