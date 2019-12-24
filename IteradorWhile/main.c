29e4d769ec5a1dada74916a80ae00b22a092e0e7
Hector Franco Torres <tor6.franco6@hotmail.com>
2019-12-23 19:32:06 -0600

    Iterador while

---
+++ IteradorWhile/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Declaracion de variable (iterador)
+    int i = 1;
+
+    // Imprimir # en 5 filas con while
+    while(i <= 5){
+        printf("#\n");
+        i++;
+    }
+
+    return 0;
+}
